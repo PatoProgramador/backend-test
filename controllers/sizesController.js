57eb2decd11d91c64c5e226294c7b53ab4676472
PatoProgramador <spartan1171947@hotmail.com>
2023-03-14 20:04:59 -0500

    Solution 1

--- controllers/sizesController.js
+++ controllers/sizesController.js
@@ -1,5 +1,37 @@
 const getSizes = (req, res) => {
-    res.status(200).json({"message": "Holis"})
+    const { groups } = req.body;
+    try {
+        const groupsArr = groups.split(",").map((num) => {
+            return Number(num);
+        })
+        
+        let sumaDePasajeros = 0;
+        groupsArr.forEach(function (a) { sumaDePasajeros += a; });
+
+        let grupoMinimo = 0;
+        groupsArr.forEach(function (a) { if (a > grupoMinimo) grupoMinimo = a })
+
+        const sizes = [];
+        for (let i = grupoMinimo; i <= sumaDePasajeros; i++) {
+            let acumulador = i;
+            let aux = false;
+            for (let j = 0; j < groupsArr.length; j++) {
+                if (groupsArr[j] > acumulador) break
+                if (groupsArr[j] <= acumulador) acumulador -= groupsArr[j];
+                if (acumulador === 0 && j === groupsArr.length - 1) {
+                    aux = true;
+                }
+                if (acumulador === 0) acumulador = i;
+            }
+            if (aux === true) {
+                sizes.push(i)
+            }
+        }
+        let sizesString = sizes.join();
+        res.status(200).json({ "sizes": sizesString })
+    } catch (error) {
+        res.status(500).json({ "message": error });
+    }
 };
 
 module.exports = { getSizes };
